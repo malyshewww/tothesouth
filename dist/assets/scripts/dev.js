0a9632dc086c4833d15f28ab12774d51dc6fa7b0
Alexey <lxmlshv97@mail.ru>
2024-09-07 15:29:42 +0300

    add new page, small edits for filter and aside-form mobile

---
+++ dist/assets/scripts/dev.js
@@ -0,0 +1,1 @@
+import{a as m}from"../chunks/chunk-EDJVN5NE.js";import"../chunks/chunk-JWGY7WOG.js";import"../chunks/chunk-7PZ3I4MT.js";m();
---
+++ src/scripts/dev.ts
@@ -0,0 +1,5 @@
+import './modules/forms';
+import './modules/dropdown';
+
+import maskPhone from './modules/mask-phone';
+maskPhone();
